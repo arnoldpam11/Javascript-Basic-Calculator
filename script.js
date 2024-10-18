ed8366f83ecffb785f6f432b736db4d572a66728
Arnold Pampelon <pampelon.arnold11@gmail.com>
2024-10-17 17:09:02 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Functions
+function addition(x, y) {
+    return x + y;
+}
+
+function subtraction(x, y) {
+    return x - y;
+}
+
+function multiplication(x, y) {
+    return x * y;
+}
+
+function division(x, y) {
+    if (y === 0) {
+        return 'Error! Zero Division cannot be divided...';
+    }
+    return x / y;
+}
+
+while (true) {
+    // Inputs Validation
+    let operation = parseInt(prompt('Choose your operation first:\n 1.)Addition\n 2.)Subtraction\n 3.) Multiplication\n 4.) Division '));
+    let num1 = parseFloat(prompt('Enter your first number: '));
+    let num2 = parseFloat(prompt('Enter your second number: '));
+    // Calculate
+    let result;
+    if (operation === 1) {
+        result = addition(num1, num2);
+        alert(`${num1} + ${num2} = ${result}`);
+    } else if (operation === 2) {
+        result = subtraction(num1, num2);
+        alert(`${num1} - ${num2} = ${result}`);
+    } else if (operation === 3) {
+        result = multiplication(num1, num2);
+        alert(`${num1} * ${num2} = ${result}`);
+    } else if (operation === 4) {
+        if (num2 === 0) {
+            alert('Error! Zero Division cannot be divided...');
+        } else {
+            result = division(num1, num2);
+            alert(`${num1} / ${num2} = ${result}`);
+        }
+    } else {
+        alert('Sorry, Invalid Operation! Please try again....');
+    }
+}
